fab20e8afcd9f08909efe73d89f2ef732833f99d
Ryan <rnleach@users.noreply.github.com>
2019-12-12 20:30:05 -0700

    Remove join handles.

--- src/bin/bufcli/builder/data.rs
+++ src/bin/bufcli/builder/data.rs
@@ -13,7 +13,7 @@ use std::{
     error::Error,
     iter::FromIterator,
     path::Path,
-    thread::{self, JoinHandle},
+    thread::{self},
 };
 use threadpool;
 
@@ -33,17 +33,12 @@ pub(super) fn build(args: CmdLineArgs) -> Result<HashSet<(Site, Model)>, Box<dyn
     let (stats_snd, stats_rcv) = channel::bounded::<StatsRecord>(CAPACITY);
 
     // Hook everything together
-    let stats_jh = start_stats_thread(root, stats_rcv)?;
-
-    let (ep_jh, total_num) = start_entry_point_thread(args, entry_point_snd)?;
-    let load_jh = start_load_thread(root, load_requests_rcv, parse_requests_snd)?;
-
-    let parser_jh = start_parser_thread(parse_requests_rcv, cli_requests_snd)?;
-
-    let cli_stats_jh =
-        start_cli_stats_thread(cli_requests_rcv, loc_requests_snd, stats_snd.clone())?;
-
-    let loc_stats_jh = start_location_stats_thread(loc_requests_rcv, comp_notify_snd, stats_snd)?;
+    start_stats_thread(root, stats_rcv, comp_notify_snd.clone())?;
+    let total_num = start_entry_point_thread(args, entry_point_snd)?;
+    start_load_thread(root, load_requests_rcv, parse_requests_snd)?;
+    start_parser_thread(parse_requests_rcv, cli_requests_snd)?;
+    start_cli_stats_thread(cli_requests_rcv, loc_requests_snd, stats_snd.clone())?;
+    start_location_stats_thread(loc_requests_rcv, comp_notify_snd, stats_snd)?;
 
     let mut site_model_pairs = HashSet::new();
 
@@ -55,6 +50,10 @@ pub(super) fn build(args: CmdLineArgs) -> Result<HashSet<(Site, Model)>, Box<dyn
             PopulateCompleted { num, site, model } => {
                 pb.set(num as u64);
                 site_model_pairs.insert((site, model));
+            }
+            TerminateThread => {
+                // Signal that the stats thread is done, so everything else must also be done.
+                pb.finish();
             }
             DataError {
                 num,
@@ -81,34 +80,41 @@ pub(super) fn build(args: CmdLineArgs) -> Result<HashSet<(Site, Model)>, Box<dyn
         }
     }
 
-    pb.finish();
-
-    println!("Waiting for populate service threads to shut down.");
-    ep_jh.join().expect("Error joining entry point thread.")?;
-    load_jh.join().expect("Error joining load thread.")?;
-    parser_jh.join().expect("Error joining parsing thread.")?;
-    cli_stats_jh
-        .join()
-        .expect("Error joining cli_stats thread.")?;
-    loc_stats_jh
-        .join()
-        .expect("Error joining location thread.")?;
-    stats_jh.join().expect("Error joining stats thread.")?;
-    println!("Done populating climate data.");
-
     Ok(site_model_pairs)
 }
 
-// A join handle for the entry point thread, which returns and error with a string or nothing. It
-// is also packaged with the total count of soundings that need to be possibly added to the
-// archive index. Some (many) will likely already be there and can be skipped, but we need to
-// check each and every one.
-type EntryPointData = (JoinHandle<Result<(), String>>, i64);
+macro_rules! assign_or_bail {
+    ($res:expr, $channel:ident) => {
+        match $res {
+            Ok(val) => val,
+            Err(err) => {
+                $channel
+                    .send(DataPopulateMsg::ThreadError(err.to_string()))
+                    .unwrap_or_else(|err| {
+                        eprintln!("Broken channel, returning from thread with error: {}", err)
+                    });
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! send_or_bail {
+    ($msg:ident, $channel:ident) => {
+        match $channel.send($msg) {
+            Ok(()) => {}
+            Err(err) => {
+                eprintln!("Broken channel with error: {}", err);
+                return;
+            }
+        }
+    };
+}
 
 fn start_entry_point_thread(
     args: CmdLineArgs,
     entry_point_snd: Sender<DataPopulateMsg>,
-) -> Result<EntryPointData, Box<dyn Error>> {
+) -> Result<i64, Box<dyn Error>> {
     let arch = Archive::connect(&args.root)?;
     let sites = args
         .sites
@@ -121,29 +127,27 @@ fn start_entry_point_thread(
         total += arch.count_init_times(&site.id, model)?;
     }
 
-    let jh = thread::Builder::new().name("Generator".to_string()).spawn(
-        move || -> Result<(), String> {
+    thread::Builder::new()
+        .name("Generator".to_string())
+        .spawn(move || -> () {
             let force_rebuild = args.operation == "build";
 
-            let arch = Archive::connect(&args.root).map_err(|err| err.to_string())?;
-
-            let climo_db = ClimoDB::connect_or_create(&args.root).map_err(|err| err.to_string())?;
-            let mut climo_db =
-                ClimoPopulateInterface::initialize(&climo_db).map_err(|err| err.to_string())?;
+            let arch = assign_or_bail!(Archive::connect(&args.root), entry_point_snd);
+            let climo_db = assign_or_bail!(ClimoDB::connect_or_create(&args.root), entry_point_snd);
+            let mut climo_db = assign_or_bail!(
+                ClimoPopulateInterface::initialize(&climo_db),
+                entry_point_snd
+            );
 
             let mut counter = 0;
             for (site, &model) in iproduct!(&sites, &args.models) {
-                let init_times = arch
-                    .init_times(&site.id, model)
-                    .map_err(|err| err.to_string())?;
+                let init_times = assign_or_bail!(arch.init_times(&site.id, model), entry_point_snd);
                 let init_times: HashSet<NaiveDateTime> = HashSet::from_iter(init_times);
 
                 let done_times = if !force_rebuild {
-                    HashSet::from_iter(
-                        climo_db
-                            .valid_times_for(&site, model)
-                            .map_err(|err| err.to_string())?,
-                    )
+                    let iter =
+                        assign_or_bail!(climo_db.valid_times_for(&site, model), entry_point_snd);
+                    HashSet::from_iter(iter)
                 } else {
                     HashSet::new()
                 };
@@ -159,31 +163,28 @@ fn start_entry_point_thread(
                         site: site.clone(),
                         num: counter,
                     };
-                    entry_point_snd
-                        .send(message)
-                        .map_err(|err| err.to_string())?;
+
+                    send_or_bail!(message, entry_point_snd);
                 }
 
                 counter += init_times.len() - small_counter;
             }
-            Ok(())
-        },
-    )?;
+        })?;
 
-    Ok((jh, total))
+    Ok(total)
 }
 
 fn start_load_thread(
     root: &Path,
     load_requests_rcv: Receiver<DataPopulateMsg>,
     parse_requests_snd: Sender<DataPopulateMsg>,
-) -> Result<JoinHandle<Result<(), String>>, Box<dyn Error>> {
+) -> Result<(), Box<dyn Error>> {
     let root = root.to_path_buf();
 
-    let jh = thread::Builder::new()
+    thread::Builder::new()
         .name("FileLoader".to_string())
-        .spawn(move || -> Result<(), String> {
-            let arch = Archive::connect(root).map_err(|err| err.to_string())?;
+        .spawn(move || -> () {
+            let arch = assign_or_bail!(Archive::connect(root), parse_requests_snd);
 
             for load_req in load_requests_rcv {
                 let message = match load_req {
@@ -211,23 +212,21 @@ fn start_load_thread(
                     message => message,
                 };
 
-                parse_requests_snd
-                    .send(message)
-                    .map_err(|err| err.to_string())?;
+                send_or_bail!(message, parse_requests_snd);
             }
-            Ok(())
+            ()
         })?;
 
-    Ok(jh)
+    Ok(())
 }
 
 fn start_parser_thread(
     parse_requests: Receiver<DataPopulateMsg>,
     cli_requests: Sender<DataPopulateMsg>,
-) -> Result<JoinHandle<Result<(), String>>, Box<dyn Error>> {
-    let jh = thread::Builder::new()
+) -> Result<(), Box<dyn Error>> {
+    thread::Builder::new()
         .name("SoundingParser".to_string())
-        .spawn(move || -> Result<(), String> {
+        .spawn(move || -> () {
             for msg in parse_requests {
                 if let DataPopulateMsg::Parse {
                     num,
@@ -247,7 +246,7 @@ fn start_parser_thread(
                                 valid_time: init_time,
                                 msg: err.to_string(),
                             };
-                            cli_requests.send(message).map_err(|err| err.to_string())?;
+                            send_or_bail!(message, cli_requests);
                             continue;
                         }
                     };
@@ -266,7 +265,7 @@ fn start_parser_thread(
                                 valid_time,
                                 snd: Box::new(snd),
                             };
-                            cli_requests.send(message).map_err(|err| err.to_string())?;
+                            send_or_bail!(message, cli_requests);
                         } else {
                             let message = DataPopulateMsg::DataError {
                                 num,
@@ -275,28 +274,27 @@ fn start_parser_thread(
                                 valid_time: init_time,
                                 msg: "No valid time".to_string(),
                             };
-                            cli_requests.send(message).map_err(|err| err.to_string())?;
+
+                            send_or_bail!(message, cli_requests);
                         }
                     }
                 } else {
-                    cli_requests.send(msg).map_err(|err| err.to_string())?;
+                    send_or_bail!(msg, cli_requests);
                 }
             }
-
-            Ok(())
         })?;
 
-    Ok(jh)
+    Ok(())
 }
 
 fn start_cli_stats_thread(
     cli_requests: Receiver<DataPopulateMsg>,
     location_requests: Sender<DataPopulateMsg>,
     climo_update_requests: Sender<StatsRecord>,
-) -> Result<JoinHandle<Result<(), String>>, Box<dyn Error>> {
-    let jh = thread::Builder::new()
+) -> Result<(), Box<dyn Error>> {
+    thread::Builder::new()
         .name("CliStatsBuilder".to_string())
-        .spawn(move || -> Result<(), String> {
+        .spawn(move || -> () {
             const POOL_SIZE: usize = 12;
 
             let pool = threadpool::Builder::new()
@@ -326,9 +324,7 @@ fn start_cli_stats_thread(
                                     valid_time,
                                     &snd,
                                 );
-                                local_update_requests
-                                    .send(message)
-                                    .expect("Error sending message.");
+                                send_or_bail!(message, local_update_requests);
                             }
 
                             let message = DataPopulateMsg::Location {
@@ -338,34 +334,28 @@ fn start_cli_stats_thread(
                                 valid_time,
                                 snd,
                             };
-                            local_location_requests
-                                .send(message)
-                                .expect("Error sending message.");
+                            send_or_bail!(message, local_location_requests);
                         } else {
-                            local_location_requests
-                                .send(msg)
-                                .expect("Error sending message.");
+                            send_or_bail!(msg, local_location_requests);
                         }
                     }
                 });
             }
 
             pool.join();
-
-            Ok(())
         })?;
 
-    Ok(jh)
+    Ok(())
 }
 
 fn start_location_stats_thread(
     location_requests: Receiver<DataPopulateMsg>,
     completed_notification: Sender<DataPopulateMsg>,
     climo_update_requests: Sender<StatsRecord>,
-) -> Result<JoinHandle<Result<(), String>>, Box<dyn Error>> {
-    let jh = thread::Builder::new()
+) -> Result<(), Box<dyn Error>> {
+    thread::Builder::new()
         .name("LocationUpdater".to_string())
-        .spawn(move || -> Result<(), String> {
+        .spawn(move || -> () {
             for msg in location_requests {
                 if let DataPopulateMsg::Location {
                     num,
@@ -388,13 +378,11 @@ fn start_location_stats_thread(
                             &snd,
                         ) {
                             Ok(msg) => {
-                                climo_update_requests
-                                    .send(msg)
-                                    .map_err(|err| err.to_string())?;
+                                send_or_bail!(msg, climo_update_requests);
 
-                                completed_notification
-                                    .send(DataPopulateMsg::PopulateCompleted { num, site, model })
-                                    .map_err(|err| err.to_string())?;
+                                let message =
+                                    DataPopulateMsg::PopulateCompleted { num, site, model };
+                                send_or_bail!(message, completed_notification);
                             }
                             Err(site) => {
                                 let message = DataPopulateMsg::DataError {
@@ -404,46 +392,47 @@ fn start_location_stats_thread(
                                     valid_time,
                                     msg: "Missing location information".to_string(),
                                 };
-                                completed_notification
-                                    .send(message)
-                                    .map_err(|err| err.to_string())?;
+                                send_or_bail!(message, completed_notification);
                             }
                         }
                     }
                 } else {
-                    completed_notification
-                        .send(msg)
-                        .map_err(|err| err.to_string())?;
+                    send_or_bail!(msg, completed_notification);
                 }
             }
 
-            Ok(())
+            ()
         })?;
 
-    Ok(jh)
+    Ok(())
 }
 
 fn start_stats_thread(
     root: &Path,
     stats_rcv: Receiver<StatsRecord>,
-) -> Result<JoinHandle<Result<(), String>>, Box<dyn Error>> {
+    comp_notify_snd: Sender<DataPopulateMsg>,
+) -> Result<(), Box<dyn Error>> {
     let root = root.to_path_buf();
 
-    let jh = thread::Builder::new()
+    thread::Builder::new()
         .name("ClimoWriter".to_string())
-        .spawn(move || -> Result<(), String> {
-            let climo_db = ClimoDB::connect_or_create(&root).map_err(|err| err.to_string())?;
-            let mut climo_db =
-                ClimoPopulateInterface::initialize(&climo_db).map_err(|err| err.to_string())?;
+        .spawn(move || -> () {
+            let climo_db = assign_or_bail!(ClimoDB::connect_or_create(&root), comp_notify_snd);
+            let mut climo_db = assign_or_bail!(
+                ClimoPopulateInterface::initialize(&climo_db),
+                comp_notify_snd
+            );
 
             for msg in stats_rcv {
-                climo_db.add(msg).map_err(|err| err.to_string())?;
+                let _ = assign_or_bail!(climo_db.add(msg), comp_notify_snd);
             }
 
-            Ok(())
+            comp_notify_snd
+                .send(DataPopulateMsg::TerminateThread)
+                .expect("Error sending terminate thread.");
         })?;
 
-    Ok(jh)
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -487,4 +476,6 @@ enum DataPopulateMsg {
         valid_time: NaiveDateTime,
         msg: String,
     },
+    ThreadError(String),
+    TerminateThread,
 }
--- src/bin/bufcli/builder/deciles.rs
+++ src/bin/bufcli/builder/deciles.rs
@@ -8,7 +8,7 @@ use std::{
     collections::HashSet,
     error::Error,
     path::Path,
-    thread::{self, JoinHandle},
+    thread::{self},
 };
 use threadpool;
 
@@ -19,7 +19,7 @@ pub(super) fn build(
     println!("Updating distributions.");
     let total_num = site_model_pairs.len();
     let (to_main, from_cdf_builder) = channel::bounded::<DecilesBuilderMsg>(CAPACITY);
-    let (jh_calc, jh_db_add) = start_deciles_builder(site_model_pairs, to_main, archive_root)?;
+    start_deciles_builder(site_model_pairs, to_main, archive_root)?;
 
     let mut pb = ProgressBar::new(total_num as u64);
     for msg in from_cdf_builder {
@@ -30,128 +30,131 @@ pub(super) fn build(
 
         pb.inc();
     }
+    pb.finish();
     println!("Done updating distributions.");
-
-    println!("Waiting for CDF service threads to shut down.");
-    jh_calc
-        .join()
-        .expect("Error joining CDF calculation threads.")?;
-    jh_db_add
-        .join()
-        .expect("Error joining CDF db add thread.")?;
 
     Ok(())
 }
 
-// This is a pair of join handles for two threads started to build CDFs. The first one is the
-// thread that loads the vecs and dispatches their calculations to a thread pool, which that
-// thread manages so we don't need to worry about here. That thread pool dispatches requests to add
-// Deciles to the climo_stats database, which is the second thread that is returned here. The
-// caller of start_deciles_builder needs to wait for both of these threads to finish.
-type DecilesBuilderHandles = (
-    JoinHandle<Result<(), String>>,
-    JoinHandle<Result<(), String>>,
-);
+macro_rules! assign_or_bail {
+    ($res:expr, $channel:ident) => {
+        match $res {
+            Ok(val) => val,
+            Err(err) => {
+                $channel
+                    .send(DecilesBuilderMsg::ThreadError(err.to_string()))
+                    .unwrap_or_else(|err| {
+                        eprintln!("Broken channel, returning from thread with error: {}", err)
+                    });
+                return;
+            }
+        }
+    };
+}
+
+macro_rules! send_or_bail {
+    ($msg:ident, $channel:ident) => {
+        match $channel.send($msg) {
+            Ok(()) => {}
+            Err(err) => {
+                eprintln!("Broken channel with error: {}", err);
+                return;
+            }
+        }
+    };
+}
 
 fn start_deciles_builder(
     set: HashSet<(Site, Model)>,
     to_main: Sender<DecilesBuilderMsg>,
     root: &Path,
-) -> Result<DecilesBuilderHandles, Box<dyn Error>> {
+) -> Result<(), Box<dyn Error>> {
     let (data_snd, cdf_builder_receiver) = channel::bounded::<DecilesBuilderMsg>(CAPACITY);
     let (db_add_send, db_add_receiver) = channel::bounded::<DecilesBuilderMsg>(CAPACITY);
 
     let local_root = root.to_path_buf();
-    let jh_producer =
-        thread::Builder::new()
-            .name("CDFMaster".into())
-            .spawn(move || -> Result<(), String> {
-                const POOL_SIZE: usize = 12;
+    let local_to_main = to_main.clone();
+    thread::Builder::new()
+        .name("CDFMaster".into())
+        .spawn(move || -> () {
+            const POOL_SIZE: usize = 12;
 
-                // Make a pool of threads that take a vec and build a CDF distribution out of them
-                let pool = threadpool::Builder::new()
-                    .num_threads(POOL_SIZE)
-                    .thread_name("DecilesBuilder".to_string())
-                    .build();
+            let to_main = local_to_main;
 
-                for _ in 0..POOL_SIZE {
-                    let local_cdf_builder_receiver = cdf_builder_receiver.clone();
-                    let local_db_add_send = db_add_send.clone();
+            // Make a pool of threads that take a vec and build a CDF distribution out of them
+            let pool = threadpool::Builder::new()
+                .num_threads(POOL_SIZE)
+                .thread_name("DecilesBuilder".to_string())
+                .build();
 
-                    pool.execute(move || {
-                        for msg in local_cdf_builder_receiver {
-                            if let DecilesBuilderMsg::HourlyCDFData { site, model, data } = msg {
-                                let cdfs = ClimoCDFBuilderInterface::create_deciles(&data);
-                                let msg = DecilesBuilderMsg::CDFDBAdd { site, model, cdfs };
+            for _ in 0..POOL_SIZE {
+                let local_cdf_builder_receiver = cdf_builder_receiver.clone();
+                let local_db_add_send = db_add_send.clone();
 
-                                local_db_add_send.send(msg).unwrap();
-                            } else {
-                                // Just forward the message and we'll deal with it later.
-                                local_db_add_send.send(msg).unwrap();
-                            }
+                pool.execute(move || {
+                    for msg in local_cdf_builder_receiver {
+                        if let DecilesBuilderMsg::HourlyCDFData { site, model, data } = msg {
+                            let cdfs = ClimoCDFBuilderInterface::create_deciles(&data);
+                            let msg = DecilesBuilderMsg::CDFDBAdd { site, model, cdfs };
+
+                            local_db_add_send.send(msg).unwrap();
+                        } else {
+                            // Just forward the message and we'll deal with it later.
+                            local_db_add_send.send(msg).unwrap();
                         }
-                    });
-                }
-                drop(cdf_builder_receiver);
-                drop(db_add_send);
+                    }
+                });
+            }
+            drop(cdf_builder_receiver);
+            drop(db_add_send);
 
-                // Load the vec of data for each site/model pair, and send it to the pool
-                let climo_db =
-                    ClimoDB::connect_or_create(&local_root).map_err(|err| err.to_string())?;
-                let mut climo_db = ClimoCDFBuilderInterface::initialize(&climo_db)
-                    .map_err(|err| err.to_string())?;
-                for (site, model) in set.into_iter() {
-                    let data = climo_db
-                        .load_all_data(&site, model)
-                        .map_err(|err| err.to_string())?;
+            // Load the vec of data for each site/model pair, and send it to the pool
+            let climo_db = assign_or_bail!(ClimoDB::connect_or_create(&local_root), to_main);
+            let mut climo_db =
+                assign_or_bail!(ClimoCDFBuilderInterface::initialize(&climo_db), to_main);
+            for (site, model) in set.into_iter() {
+                let data = assign_or_bail!(climo_db.load_all_data(&site, model), to_main);
+                let msg = DecilesBuilderMsg::HourlyCDFData { site, model, data };
+                send_or_bail!(msg, data_snd);
+            }
+            drop(data_snd);
 
-                    let msg = DecilesBuilderMsg::HourlyCDFData { site, model, data };
-
-                    data_snd.send(msg).map_err(|err| err.to_string())?;
-                }
-                drop(data_snd);
-
-                pool.join();
-
-                Ok(())
-            })?;
+            pool.join();
+        })?;
 
     // Make a thread the receives completed CDFS and inserts them in the db
     let local_root = root.to_path_buf();
-    let jh_cdf_db_adder =
-        thread::Builder::new()
-            .name("CDFDbAdder".into())
-            .spawn(move || -> Result<(), String> {
-                let climo_db =
-                    ClimoDB::connect_or_create(&local_root).map_err(|err| err.to_string())?;
-                let mut climo_db = ClimoCDFBuilderInterface::initialize(&climo_db)
-                    .map_err(|err| err.to_string())?;
+    thread::Builder::new()
+        .name("CDFDbAdder".into())
+        .spawn(move || -> () {
+            let climo_db = assign_or_bail!(ClimoDB::connect_or_create(&local_root), to_main);
+            let mut climo_db =
+                assign_or_bail!(ClimoCDFBuilderInterface::initialize(&climo_db), to_main);
 
-                for msg in db_add_receiver {
-                    if let DecilesBuilderMsg::CDFDBAdd { site, model, cdfs } = msg {
-                        for (day_of_year, hour, hdw, dt, meters, dcape) in cdfs.into_iter() {
-                            climo_db
-                                .add_to_db(
-                                    &site,
-                                    model,
-                                    day_of_year,
-                                    hour,
-                                    (hdw, dt, meters, dcape),
-                                )
-                                .map_err(|err| err.to_string())?;
-                        }
-                        let msg = DecilesBuilderMsg::CDFStatsCompleted;
-                        to_main.send(msg).map_err(|err| err.to_string())?;
-                    } else {
-                        // Just forward the message and we'll deal with it later.
-                        to_main.send(msg).map_err(|err| err.to_string())?;
+            for msg in db_add_receiver {
+                if let DecilesBuilderMsg::CDFDBAdd { site, model, cdfs } = msg {
+                    for (day_of_year, hour, hdw, dt, meters, dcape) in cdfs.into_iter() {
+                        let _ = assign_or_bail!(
+                            climo_db.add_to_db(
+                                &site,
+                                model,
+                                day_of_year,
+                                hour,
+                                (hdw, dt, meters, dcape)
+                            ),
+                            to_main
+                        );
                     }
+                    let msg = DecilesBuilderMsg::CDFStatsCompleted;
+                    send_or_bail!(msg, to_main);
+                } else {
+                    // Just forward the message and we'll deal with it later.
+                    send_or_bail!(msg, to_main);
                 }
+            }
+        })?;
 
-                Ok(())
-            })?;
-
-    Ok((jh_producer, jh_cdf_db_adder))
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -173,5 +176,6 @@ enum DecilesBuilderMsg {
             Deciles,
         )>,
     },
+    ThreadError(String),
     CDFStatsCompleted,
 }
